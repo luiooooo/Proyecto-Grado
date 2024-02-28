71812622d1114f63cb0f822662532aa822b18dcf
th3n4 <th3n4t4@hotmail.com>
2024-02-28 17:16:25 -0400

    Arreglo error linea 63, creacion de boton procesar

--- Frontend/main_window.py
+++ Frontend/main_window.py
@@ -1,8 +1,14 @@
 import tkinter as tk
 
+""" Significado de comentarios
+# desarrollo
+#---Titulo
+#--Subtitulo
+"""
+
 #PESTANA BASE
 
-#-----Aqui creamos la ventana principal
+#---Aqui creamos la ventana principal
 VentanaPrincipal = tk.Tk()
 
 #Titulo que aparece arriba de la ventana principal 
@@ -13,19 +19,21 @@ VentanaPrincipal.geometry("1600x900")
 
 VentanaPrincipal.resizable(width=False, height=False)
 
-#-----Fin PESTANA BASE
+#---Fin PESTANA BASE
 
-#-----Comando de Botones
+#---Comando de Botones
 
 def BNTFunc_Texto_Original():
     print("PRESIONAR")
 def BTNFunc_Texto_Plano():
     x = 5
 
-#-----Fin Comando de Botones
+#---Fin Comando de Botones
 
-#-----Botones
-
+#---Botones
+    
+#--Botones para ver texto
+    
 #159 Distancia de botones
 BTN_Ver_Texto_Original = tk.Button(VentanaPrincipal, text="TEXTO ORIGINAL", command=BNTFunc_Texto_Original, width=16, height=1, font=("Times New Roman",12, "bold"))
 BTN_Ver_Texto_Original.place(x=12,y=15, relwidth=0.1)
@@ -48,28 +56,38 @@ BTN_Ver_Completo.place(x=807,y=15, relwidth=0.1)
 BTN_Ver_Reporte = tk.Button(VentanaPrincipal, text="REPORTE", command=BNTFunc_Texto_Original, width=16, height=1, font=("Times New Roman",12, "bold"))
 BTN_Ver_Reporte.place(x=966,y=15, relwidth=0.1)
 
-#-----Fin Botones
+#--Fin Botones para ver texto
 
-#-----MARCOS
+#--BOTON PROCESAR TEXTO
 
-MARCOBLANCO = tk.Frame(VentanaPrincipal, bg='white', width=50, height=50)
-MARCOBLANCO.place(x=1200, y= 20)
+BTN_PROCESAR_TEXTO = tk.Button(VentanaPrincipal, text="PROCESAR", width=18, height=2, font=("Times New Roman", 16, "bold"))
+BTN_PROCESAR_TEXTO.place(x=1279, y=800)
 
-#-----FIN MARCOS
+#--FIN BOTON PROCESAR TEXTO
 
-#-----TextBoxs
+#---Fin Botones
+
+#---MARCOS
+
+MARCO_Lista = tk.Frame(VentanaPrincipal, bg='white', width=370, height=735, bd=2, relief=tk.SOLID)
+MARCO_Lista.place(x=1200, y= 45)
+
+#---FIN MARCOS
+
+#---TextBoxs
 
 #Este es el textbox principal donde se ingresara el texto
-TextBoxPRINCIPAL = tk.Text(VentanaPrincipal, padx=5, height= 39, width=123,  font=('Times New Roman', 14))
+TextBoxPRINCIPAL = tk.Text(VentanaPrincipal, padx=7, pady = 7, height= 39, width=123,  font=('Times New Roman', 14), borderwidth=2, relief=tk.SOLID)
 TextBoxPRINCIPAL.place(x=12 , y=51)
 
-#-----Fin TextBoxs
+#---Fin TextBoxs
 
-#-----LABELS
+#---LABELS
 
 LBL_LISTA = tk.Label(text="P R O Y E C T O S", font=('Times New Roman', 18, 'bold', 'underline'))
-LBL_LISTA.place(x=1200, y= 10)
-#-----FIN LABELS
+LBL_LISTA.place(x=1283, y= 10)
 
-#-----Aqui se inicializa la ventan principal
+#---FIN LABELS
+
+#---Aqui se inicializa la ventan principal
 VentanaPrincipal.mainloop()
